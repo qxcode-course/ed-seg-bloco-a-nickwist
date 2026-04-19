4e6988d6efd06677e51534ad34cb1f5cd4ecb186
Jean Nickolas <jeannickolas81@gmail.com>
2026-04-19 01:09:05 -0300

    tarefa 'gomos' concluida

--- ed/gomos/src/go/draft.go
+++ ed/gomos/src/go/draft.go
@@ -1,5 +1,69 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var quantG int
+    var mov string
+    var x []int
+    var y []int
+
+    x = make([]int, 0)
+    y = make([]int, 0)
+
+    fmt.Scan(&quantG, &mov)
+
+    for i := 0; i < quantG; i++ {
+        var z, w int
+
+        fmt.Scan(&z, &w)
+        x = append(x, z)
+        y = append(y, w)
+    }
+
+    if mov == "L" {
+        for i := quantG-1; i >= 0; i-- {
+            if i != 0 {
+                x[i] = x[i-1]
+                y[i] = y[i-1]
+            } else {
+                x[i]--
+            }
+        }
+    }
+
+    if mov == "R" {
+        for i := quantG-1; i >= 0; i-- {
+            if i != 0 {
+                x[i] = x[i-1]
+                y[i] = y[i-1]
+            } else {
+                x[i]++
+            }
+        }
+    }
+
+    if mov == "U" {
+        for i := quantG-1; i >= 0; i-- {
+            if i != 0 {
+                y[i] = y[i-1]
+                x[i] = x[i-1]
+            } else {
+                y[i]--
+            }
+        }
+    }
+
+    if mov == "D" {
+        for i := quantG-1; i >= 0; i-- {
+            if i != 0 {
+                y[i] = y[i-1]
+                x[i] = x[i-1]
+            } else {
+                y[i]++
+            }
+        }
+    }
+    
+    for i := 0; i < len(x); i++ {
+            fmt.Println(x[i], y[i])
+    }
 }
