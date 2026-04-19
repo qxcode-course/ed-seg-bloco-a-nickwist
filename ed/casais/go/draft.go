ca1400c14d5fd1c52a25cdfd6c464fc544dc17f7
Jean Nickolas <jeannickolas81@gmail.com>
2026-04-19 12:56:52 -0300

    tarefa 'casal' consertada

--- ed/casais/go/draft.go
+++ ed/casais/go/draft.go
@@ -1,26 +1,22 @@
 package main
 import "fmt"
+
 func main() {
-    var pares, i, quant, total, x int
-    var animal map[int] bool
-    animal = make(map[int] bool)
+    var pares, i, quant, x int
+    animal := make(map[int]bool)
 
-
-    fmt.Scan(&total)
     fmt.Scan(&quant)
 
     for i = 0; i < quant; i++ {
         fmt.Scan(&x)
-        animal[x] = true
+
         if animal[-x] {
             pares++
             delete(animal, -x)
-            delete(animal, x)
+        } else {
+            animal[x] = true
         }
-
-        
     }
-    fmt.Println(pares)
 
-    
+    fmt.Println(pares)
 }
