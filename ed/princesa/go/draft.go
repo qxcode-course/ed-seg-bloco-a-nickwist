f8dff9224373233034f004913082c1595d113b07
Jean Nickolas <jeannickolas81@gmail.com>
2026-05-29 22:48:12 -0300

    tarefa 'princesa v1' concluida

--- ed/princesa/go/draft.go
+++ ed/princesa/go/draft.go
@@ -1,5 +1,44 @@
 package main
+
 import "fmt"
+
+func procurar_vivo(elementos []int, pos int) int {
+    return pos % len(elementos)
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+
+    var size, pos int
+    fmt.Scan(&size, &pos)
+    pos--
+
+    elementos := make([]int, size)
+
+    for i := 0; i < size; i++ {
+        elementos[i] = i + 1
+    }
+
+    for len(elementos) > 1 {
+
+        pos = procurar_vivo(elementos, pos)
+
+        fmt.Print("[ ")
+        for i := 0; i < len(elementos); i++ {
+            if i == pos {
+                fmt.Print(elementos[i], "> ")
+            } else {
+                fmt.Print(elementos[i], " ")
+            }
+        }
+        fmt.Println("]")
+
+        prox := (pos + 1) % len(elementos)
+
+        elementos = append(elementos[:prox], elementos[prox+1:]...)
+
+        pos = procurar_vivo(elementos, prox)
+    }
+    
+    fmt.Printf("[ %d> ]\n", elementos[0])
+
 }
