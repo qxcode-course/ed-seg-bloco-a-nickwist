83af70c637e62aac0494291f39dabcd0653d60b3
Jean Nickolas <jeannickolas81@gmail.com>
2026-06-03 02:04:08 -0300

    tarefa "buscas" concluida

--- ed/buscas/src/go/draft.go
+++ ed/buscas/src/go/draft.go
@@ -1,5 +1,41 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var busca, consulta []string
+    var res []int
+    var quantbusca, quantconsulta int
+
+    fmt.Scan(&quantbusca)
+
+    for i := 0; i < quantbusca; i++ {
+        var buscas string
+        fmt.Scan(&buscas)
+        busca = append(busca, buscas)
+    }
+
+    fmt.Scan(&quantconsulta) 
+
+    for i := 0; i < quantconsulta; i++ {
+        var consultas string
+        fmt.Scan(&consultas)
+        consulta = append(consulta, consultas)
+    }
+
+    for i := 0; i < len(consulta); i++ {
+        quantidade := 0
+        for j := 0; j < len(busca); j++ {
+            if consulta[i] == busca[j] {
+                quantidade++
+            }
+        }
+        res = append(res, quantidade)
+    }
+
+    for i := 0; i < len(res); i++ {
+        if i != len(res)- 1 {
+            fmt.Printf("%d ", res[i])
+        } else {
+            fmt.Println(res[i])
+        }
+    }
 }
