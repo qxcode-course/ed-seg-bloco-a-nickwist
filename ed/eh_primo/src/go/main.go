af11c6c089ce4a6b65e1f5bf277dc67c3b6b99b6
nickwist <jeannickolas81@gmail.com>
2026-06-04 21:15:38 +0000

    tarefa "eh_primo" concluida

---
+++ ed/eh_primo/src/go/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// x: número que está sendo testado
+// div: divisor que está sendo testado
+func eh_primo(x int, div int) bool {
+
+	if x < 2 {
+		return false
+	}
+
+	if div == x {
+		return true
+	}
+
+	if x%2 == 0 {
+		return false
+	}
+
+	if eh_primo(x, div+1) {
+		return true
+	}
+
+	return false
+}
+
+func main() {
+	var x int
+	fmt.Scan(&x)
+	if eh_primo(x, 2) {
+		fmt.Println("true")
+	} else {
+		fmt.Println("false")
+	}
+}
