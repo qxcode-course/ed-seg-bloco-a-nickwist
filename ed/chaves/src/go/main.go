41a5ba2e236cc633e10574ec1ce581bdb60e973a
nickwist <jeannickolas81@gmail.com>
2026-07-05 01:27:32 +0000

    tarefa "chaves" concluida

--- ed/chaves/src/go/main.go
+++ ed/chaves/src/go/main.go
@@ -1,7 +1,30 @@
 package main
 
 import (
+	"fmt"
 )
 
 func main() {
+	q := NewQueue[rune]()
+	var mm, nn int
+
+	for i := 'A'; i <= 'P'; i++ {
+		q.Enqueue(i)
+	}
+
+	for q.items.Len() != 1 {
+		primeiro := q.Dequeue()
+		segundo := q.Dequeue()
+
+		fmt.Scan(&mm, &nn)
+
+		if mm > nn {
+			q.Enqueue(primeiro)
+		} else if nn > mm {
+			q.Enqueue(segundo)
+		}
+
+	}
+
+	fmt.Printf("%c\n", q.Dequeue())
 }
