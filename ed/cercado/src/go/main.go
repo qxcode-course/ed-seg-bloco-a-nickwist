1a2179df23b0cea282d3f9b51bfa573292f2a6e7
nickwist <jeannickolas81@gmail.com>
2026-07-10 20:56:54 +0000

    tarefa "cercado" concluida

--- ed/cercado/src/go/main.go
+++ ed/cercado/src/go/main.go
@@ -6,9 +6,45 @@ import (
 	"os"
 )
 
+func DFS(board [][]byte, i, j int) {
+	if i < 0 || j < 0 || i >= len(board) || j >= len(board[0]) {
+		return
+	}
+
+	if board[i][j] != 'O' {
+		return
+	}
+
+	board[i][j] = '.'
+
+	DFS(board, i+1, j)
+	DFS(board, i-1, j)
+	DFS(board, i, j+1)
+	DFS(board, i, j-1)
+
+}
+
 // NÃO ALTERE A ASSINATURA DA FUNÇÃO solve
 func solve(board [][]byte) {
-	_ = board
+	for i := 0; i < len(board); i++ {
+		DFS(board, i, 0)
+		DFS(board, i, len(board[0])-1)
+	}
+
+	for j := 0; j < len(board[0]); j++ {
+		DFS(board, 0, j)
+		DFS(board, len(board)-1, j)
+	}
+
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			if board[i][j] == 'O' {
+				board[i][j] = 'X'
+			} else if board[i][j] == '.' {
+				board[i][j] = 'O'
+			}
+		}
+	}
 }
 
 // NÃO ALTERE A MAIN
